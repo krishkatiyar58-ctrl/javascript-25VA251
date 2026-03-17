0e7ff876c30a448d6b2226653b8036d6369f669f
KRISH KATIYAR <krishkatiyar58@gmail.com>
2026-03-17 09:20:07 +0530

    Add files via upload

---
+++  javascript.js
@@ -0,0 +1,3 @@
+//Javascript is a cross platform object based scripting language for client and server application , it is used everywhere .
+ //loosely typed language . 
+// high level and paradigm
